f262c26376f08d82200447ba3ab0d0b1b2da3f0c
jos-cab <joseacabrera@protonmail.com>
2026-06-16 13:02:01 -0300

    remove unused votar method in Estado

--- src/main/java/edu/fiuba/algo3/modelo/jugador/Muerto.java
+++ src/main/java/edu/fiuba/algo3/modelo/jugador/Muerto.java
@@ -1,15 +1,8 @@
 package edu.fiuba.algo3.modelo.jugador;
 
-import edu.fiuba.algo3.modelo.excepciones.LosMuertosNoVotanException;
-
 public class Muerto implements Estado {
     @Override
     public boolean estaVivo() {
         return false;
-    }
-
-    @Override
-    public void votar(Jugador objetivo){
-        throw new LosMuertosNoVotanException();
     }
 }
