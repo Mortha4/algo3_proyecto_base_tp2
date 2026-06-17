5d4c538cfe72645943d2abc87b0308e0d04dbcad
LucasCascini <lcascini@fi.uba.ar>
2026-06-17 18:00:18 -0300

    Agrego diagrama de clases general

--- src/main/java/edu/fiuba/algo3/modelo/comandos/VotarPrioritarioNocturno.java
+++ src/main/java/edu/fiuba/algo3/modelo/comandos/VotarPrioritarioNocturno.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo.comandos;
 
+import edu.fiuba.algo3.modelo.excepciones.SeleccionInvalidaException;
 import edu.fiuba.algo3.modelo.fase.FaseNocturna;
 import edu.fiuba.algo3.modelo.jugador.Jugador;
 
@@ -7,7 +8,10 @@ public class VotarPrioritarioNocturno implements Command{
     private final Jugador objetivo;
     private final FaseNocturna fase;
 
-    public VotarPrioritarioNocturno(FaseNocturna fase, Jugador base, Jugador objetivo) {
+    public VotarPrioritarioNocturno(FaseNocturna fase, Jugador votante, Jugador objetivo) {
+        if((!objetivo.estaVivo()) || votante.equals(objetivo)) {
+            throw new SeleccionInvalidaException();
+        }
         this.fase = fase;
         this.objetivo = objetivo;
     }
