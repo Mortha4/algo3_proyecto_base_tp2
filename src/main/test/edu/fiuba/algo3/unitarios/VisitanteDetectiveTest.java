7a122c9bfb62be37f0d0570be991f03374608ffa
jos-cab <joseacabrera@protonmail.com>
2026-06-20 14:18:28 -0300

    Test visitors

--- src/main/test/edu/fiuba/algo3/unitarios/VisitanteDetectiveTest.java
+++ src/main/test/edu/fiuba/algo3/unitarios/VisitanteDetectiveTest.java
@@ -1,4 +1,59 @@
 package edu.fiuba.algo3.unitarios;
 
+import edu.fiuba.algo3.modelo.roles.*;
+import edu.fiuba.algo3.modelo.visitors.VisitanteDetective;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VisitanteDetectiveTest {
+    private VisitanteDetective visitante;
+
+    @BeforeEach
+    public void arrange() {
+        visitante = new VisitanteDetective();
+    }
+
+    @Test
+    public void test01UnDetectiveVeAUnMafiosoComoMafioso() {
+        // Act y Assert
+        assertEquals(new Mafioso(), visitante.visitar(new Mafioso()),
+                "Un detective debería ver a un mafioso como mafioso");
+    }
+
+    @Test
+    public void test02UnDetectiveVeAUnPadrinoComoCiudadano() {
+        // Act y Assert
+        assertEquals(new Ciudadano(), visitante.visitar(new Padrino()),
+                "Un detective debería ver a un padrino como ciudadano");
+    }
+
+    @Test
+    public void test03UnDetectiveVeAUnCiudadanoComoCiudadano() {
+        // Act y Assert
+        assertEquals(new Ciudadano(), visitante.visitar(new Ciudadano()),
+                "Un detective debería ver a un ciudadano como ciudadano");
+    }
+
+    @Test
+    public void test04UnDetectiveVeAUnMedicoComoCiudadano() {
+        // Act y Assert
+        assertEquals(new Ciudadano(), visitante.visitar(new Medico()),
+                "Un detective debería ver a un médico como ciudadano");
+    }
+
+    @Test
+    public void test05UnDetectiveVeAUnDetectiveComoCiudadano() {
+        // Act y Assert
+        assertEquals(new Ciudadano(), visitante.visitar(new Detective()),
+                "Un detective debería ver a otro detective como ciudadano");
+    }
+
+    @Test
+    public void test06UnDetectiveVeAUnSheriffComoCiudadano() {
+        // Act y Assert
+        assertEquals(new Ciudadano(), visitante.visitar(new Sheriff()),
+                "Un detective debería ver a un sheriff como ciudadano");
+    }
 }
--- src/main/test/edu/fiuba/algo3/unitarios/VisitanteMafiosoTest.java
+++ src/main/test/edu/fiuba/algo3/unitarios/VisitanteMafiosoTest.java
@@ -1,4 +1,61 @@
 package edu.fiuba.algo3.unitarios;
 
+import edu.fiuba.algo3.modelo.excepciones.NoVisibleException;
+import edu.fiuba.algo3.modelo.roles.*;
+import edu.fiuba.algo3.modelo.visitors.VisitanteMafioso;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class VisitanteMafiosoTest {
+    private VisitanteMafioso visitante;
+
+    @BeforeEach
+    public void arrange() {
+        visitante = new VisitanteMafioso();
+    }
+
+    @Test
+    public void test01UnMafiosoVeAUnMafiosoComoMafioso() {
+        // Act y Assert
+        assertEquals(new Mafioso(), visitante.visitar(new Mafioso()),
+                "Un mafioso debería ver a otro mafioso como mafioso");
+    }
+
+    @Test
+    public void test02UnMafiosoVeAUnPadrinoComoMafioso() {
+        // Act y Assert
+        assertEquals(new Mafioso(), visitante.visitar(new Padrino()),
+                "Un mafioso debería ver a un padrino como mafioso");
+    }
+
+    @Test
+    public void test03UnMafiosoNoPuedeVerAUnCiudadano() {
+        // Act y Assert
+        assertThrows(NoVisibleException.class, () -> visitante.visitar(new Ciudadano()),
+                "Un mafioso no debería poder ver el bando de un ciudadano");
+    }
+
+    @Test
+    public void test04UnMafiosoNoPuedeVerAUnMedico() {
+        // Act y Assert
+        assertThrows(NoVisibleException.class, () -> visitante.visitar(new Medico()),
+                "Un mafioso no debería poder ver el bando de un médico");
+    }
+
+    @Test
+    public void test05UnMafiosoNoPuedeVerAUnDetective() {
+        // Act y Assert
+        assertThrows(NoVisibleException.class, () -> visitante.visitar(new Detective()),
+                "Un mafioso no debería poder ver el bando de un detective");
+    }
+
+    @Test
+    public void test06UnMafiosoNoPuedeVerAUnSheriff() {
+        // Act y Assert
+        assertThrows(NoVisibleException.class, () -> visitante.visitar(new Sheriff()),
+                "Un mafioso no debería poder ver el bando de un sheriff");
+    }
 }
