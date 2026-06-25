929d6ec03ec1f3e1525bebd90b48d9d65954c897
LucasCascini <lcascini@fi.uba.ar>
2026-06-24 22:22:27 -0300

    Primer test de una partida con comportamiento.

--- src/main/test/edu/fiuba/algo3/unitarios/PartidaTest.java
+++ src/main/test/edu/fiuba/algo3/unitarios/PartidaTest.java
@@ -1,15 +1,50 @@
 package edu.fiuba.algo3.unitarios;
 
 import edu.fiuba.algo3.modelo.Partida;
+import edu.fiuba.algo3.modelo.fase.Fase;
 import edu.fiuba.algo3.modelo.jugador.Jugador;
+import edu.fiuba.algo3.modelo.roles.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PartidaTest {
+    private List<Jugador> mafiosos;
+    private List<Jugador> ciudadanos;
+    private Jugador unCiudadano;
+    private Jugador unMafioso;
+    private Jugador detective = null;
+    private Jugador medico = null;
+    private Jugador padrino;
+    private Jugador sheriff;
+    private Partida partida;
+    private Fase faseActual;
+
+    public void arrangeDe5Jugadores(){
+        partida = new Partida(List.of("1", "2", "3", "4", "5"));
+        partida.iniciar();
+        faseActual = partida.getFaseActual();
+        ciudadanos = new ArrayList<>();
+        mafiosos = new ArrayList<>();
+        for(Jugador jugador: partida.getJugadores()){
+            Rol rol = jugador.getRol();
+            if(rol instanceof Mafioso) {
+                mafiosos.add(jugador);
+            } else if (rol instanceof Ciudadano){
+                ciudadanos.add(jugador);
+            } else if (rol instanceof Detective){
+                detective = jugador;
+            } else if (rol instanceof Medico){
+                medico = jugador;
+            }
+        }
+        unCiudadano = ciudadanos.get(0);
+        unMafioso = mafiosos.get(0);
+    }
+
     @Test
     public void test01TodosLosJugadoresRecibenUnRolEnUnaPartidaDe5Jugadores(){
         // Arrange
@@ -59,5 +94,18 @@ public class PartidaTest {
             assertNotNull(jugador.verRol(jugador),
                     "Todos los jugadores deberían recibir un rol al repartir las cartas");
         }
+    }
+
+    @Test
+    public void test04TranscurreLaPrimerNocheCorrectamente(){
+        // Act
+        arrangeDe5Jugadores();
+        unMafioso.nominar(faseActual, unCiudadano);
+        for(Jugador mafia: mafiosos){
+            mafia.votar(faseActual, unCiudadano);
+        }
+        partida.cambiarFase();
+        assertFalse(unCiudadano.estaVivo(),
+                "Si todos votaron a un jugador, al cambiar de fase deberia morir.");
     }
 }
